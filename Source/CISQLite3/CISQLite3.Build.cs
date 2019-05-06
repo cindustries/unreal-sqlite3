140ace0c7d66d0e2097ea9172986053eacab0336
Squareys <squareys@googlemail.com>
2019-05-06 13:47:25 +0200

    Avoid a warning with newer UE4 versions
    Signed-off-by: Squareys <squareys@googlemail.com>

--- Source/CISQLite3/CISQLite3.Build.cs
+++ Source/CISQLite3/CISQLite3.Build.cs
@@ -1,23 +1,17 @@
 // Copyright (c) 2015 Jussi Saarivirta 2016 conflict.industries MIT License (MIT)
 
 using UnrealBuildTool;
+using System.IO;
 
 public class CISQLite3 : ModuleRules
 {
   public CISQLite3(ReadOnlyTargetRules Target) : base(Target)
   {
 
-    PublicIncludePaths.AddRange(
-      new string[] {
-        "CISQLite3/Public"
-      }
-    );
+    PublicIncludePaths.AddRange(new string[] { Path.Combine(ModuleDirectory, "Public") });
+    PrivateIncludePaths.AddRange(new string[] {  Path.Combine(ModuleDirectory, "Private") });
 
-    PrivateIncludePaths.AddRange(
-      new string[] {
-        "CISQLite3/Private"
-      }
-    );
+    PrivatePCHHeaderFile = "Private/CISQLite3PrivatePCH.h";
 
     PublicDependencyModuleNames.AddRange(
       new string[] {
